112253e920baf061459850a660bd7cd206cc486a
Doug Hughes <dhughes8906@gmail.com>
2021-10-21 23:55:49 -0400

    Adds unsubscribe form
    Adds a form for the unsubscribe form. This is currently the same
    form as is on the subscribe page. However the form is not
    modularized because the functionality of the two forms will
    diverge when they are fully fleshed out.

--- front-end/3stat-react-app/src/components/Unsubscribe.js
+++ front-end/3stat-react-app/src/components/Unsubscribe.js
@@ -1,10 +1,55 @@
+import React from 'react';
+
 function Unsubscribe() {
     return (
       <div className="unsubscribe">
         <h1>Unsubscribe</h1>
         <h3>Enter an email or phone number to unsubscribe</h3>
+        <UnsubscribeForm />
       </div>
     );
+  }
+
+  class UnsubscribeForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        email: '',
+        phone: ''
+      }
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      const name = event.target.name;
+
+      this.setState({[name]: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('Email submitted: ' + this.state.email + ' / Phone # submitted: ' + this.state.phone);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+            <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+          </label>
+          <br></br>
+          <label>
+            Phone #:
+            <input name="phone" type="text" value={this.state.phone} onChange={this.handleChange} />
+          </label>
+          <br></br>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
   }
   
   export default Unsubscribe;
