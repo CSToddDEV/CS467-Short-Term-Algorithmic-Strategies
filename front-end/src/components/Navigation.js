9bf6349b29697f831eda7f1bd805490d02ced654
Doug Hughes <dhughes8906@gmail.com>
2021-10-25 13:25:02 -0400

    Adds additional nav bar styling
    Adds color scheme and an on-hover effect to the nav bar.

--- front-end/src/components/Navigation.js
+++ front-end/src/components/Navigation.js
@@ -4,11 +4,11 @@ function Navigation() {
     return (
       <nav>
           <ul class="nav">
-              <li><NavLink exact activeClassName="selectedLink" class="navLink" to='/'>Home</NavLink></li>
-              <li><NavLink exact activeClassName="selectedLink" class="navLink" to='/about'>About</NavLink></li>
-              <li><NavLink exact activeClassName="selectedLink" class="navLink" to='/subscribe'>Subscribe</NavLink></li>
-              <li><NavLink exact activeClassName="selectedLink" class="navLink" to='/unsubscribe'>Unsubscribe</NavLink></li>
-              <li><NavLink exact activeClassName="selectedLink" class="navLink" to='/data'>Signal Data</NavLink></li>
+              <li class="navUnit"><NavLink exact activeClassName="selectedLink" class="navLink" to='/'>Home</NavLink></li>
+              <li class="navUnit"><NavLink exact activeClassName="selectedLink" class="navLink" to='/about'>About</NavLink></li>
+              <li class="navUnit"><NavLink exact activeClassName="selectedLink" class="navLink" to='/subscribe'>Subscribe</NavLink></li>
+              <li class="navUnit"><NavLink exact activeClassName="selectedLink" class="navLink" to='/unsubscribe'>Unsubscribe</NavLink></li>
+              <li class="navUnit"><NavLink exact activeClassName="selectedLink" class="navLink" to='/data'>Signal Data</NavLink></li>
           </ul>
       </nav>
     );
