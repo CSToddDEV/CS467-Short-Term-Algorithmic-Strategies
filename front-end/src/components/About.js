3c335954a67e23ea85fb183dc18dc4a02587db81
Calvin <CSTodd88@gmail.com>
2021-11-18 12:04:31 -0800

    About page updated

--- front-end/src/components/About.js
+++ front-end/src/components/About.js
@@ -1,9 +1,30 @@
 function About() {
     return (
-      <div className="about content">
+      <div className="about_content">
         <h1>About 3STAT</h1>
-        <p>The goal and purpose of the Short-term Algorithmic Stock Market Strategies (referred to as 3STAT going forward) project will be to assess and implement an algorithm to successfully trade 3X leveraged ETFs (exchange-traded funds). With guidance from the project sponsor, the algorithm will be assessed and developed against historical market data, and then implemented with current market data. The successful implementation of the algorithm will culminate in a web application that notifies the users of specific buy and sell signals.
-</p>
+        <p>
+            Inflation is actively outpacing most traditional investments.  The US bond market is falling apart.  The global economy is extremely volatile.   We see wealth being hoarded by the rich and watch as policy is created by the highest bidder.
+            <br/><br/>
+            If your money is not working for you, it is actively working against you.
+            <br/><br/>
+            Now is the rise of the Retail Investor.  Investor’s looking to take charge of their finances, their future, their hope.  But in the world of Market Makers and Big Money, many Retail Investors are looking for direction.
+            <br/><br/>
+            Welcome to 3STAT.
+            <br/><br/>
+            The 3STAT Algorithm is a momentum-based multi-resolution trading algorithm with aggressive risk management built in.  The 3STAT Algorithm focuses on a curated list of 3X ETFs, with monthly universe selection utilizing volatility.  With the leveraged power of the 3X weighted ETFS, and the aggressive risk management, the 3STAT Algorithm aims to deliver mighty gains with short-term trades.
+            <br/><br/>
+            But what sets 3STAT Algorithm apart from every other Algorithmic Trader out there is its approachability provided by the 3STAT web-app.  With an interface that is navigable even for luddites, the 3STAT web-app is designed and built to be approachable for traders of all types.  You may view the performance of the 3STAT Algorithm over select periods from the performance page.  You will also be able to see that performance compared to classic benchmarks during those same periods.  All of this is provided for free to help users make the best decision for their portfolio that they can.
+            <br/><br/>
+            If you choose to sign up and become a free subscriber, you will be emailed whenever the 3STAT Algorithm has produced a new buy or sell signal.  This allows 3STAT subscribers to choose to mimic the buy and sell patterns of the 3STAT Algorithm, and reap what rewards come with it.
+            <br/><br/><br/><br/>
+            <span className="disclaimer">A new type of Investor calls for a new type of tool.  3STAT, buy in to your future.</span>
+            <br/><br/><br/><br/><br/><br/>
+            <span className="footer">
+            3STAT is a short-term algorithmic trading strategy project by Dylan Bodvig, Doug Hughes, Ryan Patton, Calvin Todd, and Timothy Turnbaugh for the Oregon State University Fall 2021 Computer Science Capstone class.
+            <br/><br/>
+            <span className="disclaimer">Disclaimer:</span> Nothing on this website is intended to represent financial advice.  Investing in stocks, bonds, exchange traded funds, mutual funds, and money market funds involve risk of loss.  Loss of principal is possible. Some high risk investments may use leverage, which will accentuate gains & losses. A security’s or an algorithm’s past investment performance is not a guarantee or predictor of future investment performance.
+            </span>
+        </p>
       </div>
     );
   }
