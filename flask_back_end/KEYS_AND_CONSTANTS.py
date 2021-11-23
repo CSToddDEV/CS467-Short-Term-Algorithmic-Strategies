0fa096ef3195ba98cd939dd7358c6c17f22c8b96
Tim Turnbaugh <turnbaugh.tim@gmail.com>
2021-11-22 20:48:47 -0800

    Add Mail config

--- flask_back_end/KEYS_AND_CONSTANTS.py
+++ flask_back_end/KEYS_AND_CONSTANTS.py
@@ -7,3 +7,11 @@ HOST = 'localhost'
 PORT = 27017
 USERNAME = 'username'
 PASSWORD = 'password'
+
+# Mail Config Setup
+MAIL_SERVER = None
+MAIL_PORT = None
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+MAIL_USE_TLS = None
+MAIL_USE_SSL = None
